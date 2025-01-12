048ce43cd24d8c85fe4544bf079aa53af87e12ba
Nicholas Ciechanowski <nicholasaciechanowski@gmail.com>
2025-01-12 15:54:47 +1100

    feat(watch): add lock file to preview building when we don't need too

--- watch-and-build.sh
+++ watch-and-build.sh
@@ -7,9 +7,22 @@ nginx &
 echo "Running initial MkDocs build..."
 mkdocs build
 
+DELAY=2 # seconds to wait
+LOCKFILE="/tmp/mkdocs_building"
+rm -f $LOCKFILE # Clear any stale lock
+
 # Watch for changes in the /app directory, excluding the /app/site directory
 echo "Watching for changes in /app (excluding /app/site)..."
-inotifywait -m -r -e modify,create,delete --exclude '/app/site/.*' /app --format '%w%f' | while read file; do
-    echo "Change detected in $file. Rebuilding MkDocs site..."
-    mkdocs build
+inotifywait -m -r -e modify,create,delete --exclude '/app/site/.*' /app --format '%w%f' | while read -r file; do
+    echo "Change detected in $file"
+
+    if [ ! -f "$LOCKFILE" ]; then
+        touch "$LOCKFILE"
+        (
+            sleep $DELAY
+            echo "Rebuilding MkDocs site..."
+            mkdocs build
+            rm -f "$LOCKFILE"
+        ) &
+    fi
 done
